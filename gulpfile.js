adff851d6e56e92d49a032997336dbb74871af06
Alican Tokdemir <alicantokdemir@gmail.com>
2017-04-26 03:07:12 -0300

    changed stuff to docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,23 @@ gulp.task('default', ['index', 'js', 'styles', 'assets', 'watch']);
 
 gulp.task('assets', function () {
     return gulp.src('Assets/**/*')
-        .pipe(gulp.dest('www/Assets'));
+        .pipe(gulp.dest('docs/Assets'));
 })
 
 gulp.task('js', function () {
     return gulp.src(['app/*.js', 'bower_components/**/*.min.js'])
-        .pipe(gulp.dest('www'))
+        .pipe(gulp.dest('docs'))
 });
 
 gulp.task('styles', function () {
     return gulp.src('styles/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('www/styles/'));
+        .pipe(gulp.dest('docs/styles/'));
 });
 
 gulp.task('index', function () {
     return gulp.src('index.html')
-        .pipe(gulp.dest('www'))
+        .pipe(gulp.dest('docs'))
 });
 
 gulp.task('watch', function () {
